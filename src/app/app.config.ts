169cdd93ba66a2fd0d8b2bf035368ef319a0bb72
José Madrigal <jmadrigal71@gmail.com>
2024-11-24 11:11:38 +0100

    Subida

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(
+      routes,
+      withViewTransitions({
+        skipInitialTransition: true, // Para que no anime la primera vista
+        onViewTransitionCreated(trasitionInfo) {
+          console.log({ trasitionInfo });
+        }
+      }),
+    ),
+    importProvidersFrom(
+      HttpClientModule,
+    )
+  ]
 };
+
